a53660c65375a63abdb6e6fd60f12a73542dd59a
istiakAHMEDsaad <istik2020@gmail.com>
2025-02-14 02:56:34 +0600

    extract form info

--- src/page/AddCar.jsx
+++ src/page/AddCar.jsx
@@ -1,8 +1,37 @@
 import { useState } from "react";
 import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const AddCar = () => {
   const [startDate, setStartDate] = useState(new Date());
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const model = form.car_model.value;
+    const price = parseFloat(form.daily_rental_price.value);
+    const available = form.availability.value;
+    const reg_num = form.registration_num.value;
+    const features = form.features.value;
+    const image = form.car_image.value;
+    const location = form.location.value;
+    const date = startDate;
+    const description = form.description.value;
+
+    const infoRes = {
+      model,
+      price,
+      available,
+      reg_num,
+      features,
+      image,
+      location,
+      date,
+      description
+    };
+    
+    console.table(infoRes)
+  };
 
   return (
     <div className='min-h-[calc(100vh-245px)] flex justify-center items-center my-12'>
@@ -13,7 +42,7 @@ const AddCar = () => {
         </h2>
 
         {/* Form Section */}
-        <form>
+        <form onSubmit={handleSubmit}>
           {/* Single input */}
           <div className='grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2'>
             {/* Car Model */}
@@ -115,7 +144,7 @@ const AddCar = () => {
 
               {/* Date Picker Input Field */}
               <DatePicker
-                className='border p-2 rounded-md'
+                className='px-2 py-[0.6rem] rounded-md text-gray-700 border bg-white border-gray-200 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring'
                 selected={startDate}
                 onChange={(date) => setStartDate(date)}
               />
@@ -128,9 +157,9 @@ const AddCar = () => {
               Description
             </label>
             <textarea
-              className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring'
               name='description'
               id='description'
+              className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring'
             ></textarea>
           </div>
 
