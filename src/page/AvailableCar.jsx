9ae51a15c610edfab31de3828db2baad853634f9
istiakAHMEDsaad <istik2020@gmail.com>
2025-02-15 21:33:47 +0600

    search & sort function from db

--- src/page/AvailableCar.jsx
+++ src/page/AvailableCar.jsx
@@ -1,19 +1,32 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import AvailableCarsCard from '../components/AvailableCarsCard';
+import toast from 'react-hot-toast';
+import { IoGrid, IoList } from "react-icons/io5";
 
 const AvailableCar = () => {
   const [carsData, setCarsData] = useState([]);
+  const [filter, setFilter] = useState('');
+  const [search, setSearch] = useState('');
+  const [toggle, setToggle] = useState(false);
 
   useEffect(() => {
     const fetchAllCarsData = async () => {
       const { data } = await axios.get(
-        `${import.meta.env.VITE_API_URL}/all-car`
+        `${
+          import.meta.env.VITE_API_URL
+        }/all-car?filter=${filter}&search=${search}`
       );
       setCarsData(data);
     };
     fetchAllCarsData();
-  }, []);
+  }, [filter, search]);
+
+  const handleResetButton = () => {
+    setFilter('');
+    setSearch('');
+    toast.success('Reset successfully!');
+  };
 
   return (
     <div className='container mx-auto mt-5 overflow-hidden'>
@@ -25,28 +38,44 @@ const AvailableCar = () => {
           </span>
         </div>
       </div>
+
+      {/* option section */}
       <div className='flex flex-col items-center space-y-10'>
-        {/* option section */}
         <div className='flex flex-col gap-4 md:flex-row'>
           {/* Sort By Category */}
           <div>
-            <select defaultValue='Pick a color' className='select'>
-              <option disabled={true}>Pick a color</option>
-              <option>Crimson</option>
-              <option>Amber</option>
-              <option>Velvet</option>
+            <select name='category' id='category' className='select' value={filter}
+              onChange={(event)=>setFilter(event.target.value)}
+            >
+              <option value=''>Filter By Category</option>
+              <option value='ascending'>Lowest First</option>
+              <option value='descending'>Highest First</option>
+              <option value='old'>Oldest First</option>
+              <option value='new'>Newest First</option>
             </select>
           </div>
 
           {/* Search field */}
           <div className='join'>
-            <input className='input join-item' placeholder='Email' />
-            <button className='btn join-item rounded-r-full'>Subscribe</button>
+            <input
+              className='input join-item'
+              type='text'
+              name='search'
+              placeholder='Enter car model'
+              aria-label='Enter car model'
+              value={search}
+              onChange={(event)=>setSearch(event.target.value)}
+            />
+            <button className='btn join-item rounded-r-full'>Search</button>
           </div>
+
+          <div>a</div>
 
           {/* Reset */}
           <div>
-            <button className='btn'>Reset</button>
+            <button onClick={handleResetButton} className='btn'>
+              Reset
+            </button>
           </div>
         </div>
 
