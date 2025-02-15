a1beba77b8310cc64d30cd66f79e7021eec60dd5
istiakAHMEDsaad <istik2020@gmail.com>
2025-02-15 22:03:17 +0600

    show grid or list item

--- src/components/AvailableCarsCard.jsx
+++ src/components/AvailableCarsCard.jsx
@@ -1,11 +1,21 @@
 import PropTypes from 'prop-types';
 
-const AvailableCarsCard = ({car}) => {
-    const {model} = car || {};
+const AvailableCarsCard = ({ car, toggle }) => {
+  const { model } = car || {};
   return (
-    <div className='card bg-base-100 w-80 shadow-sm'>
+    // card bg-base-100 w-80 shadow-sm
+    <div
+      className={`${
+        toggle === true
+          ? 'card bg-base-100 w-80 shadow-sm'
+          : 'md:flex md:h-64 mb-4 border border-gray-200 rounded-md shadow-md items-center justify-center md:pl-2'
+      }`}
+    >
       <figure>
         <img
+          className={`${
+            toggle === false ? 'md:h-60 md:w-96 md:object-cover rounded-md' : ''
+          }`}
           src='https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp'
           alt='Shoes'
         />
@@ -25,7 +35,8 @@ const AvailableCarsCard = ({car}) => {
 };
 
 AvailableCarsCard.propTypes = {
-  car: PropTypes.object.isRequired
-}
+  car: PropTypes.object.isRequired,
+  toggle: PropTypes.bool,
+};
 
 export default AvailableCarsCard;
--- src/page/AvailableCar.jsx
+++ src/page/AvailableCar.jsx
@@ -2,13 +2,13 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import AvailableCarsCard from '../components/AvailableCarsCard';
 import toast from 'react-hot-toast';
-import { IoGrid, IoList } from "react-icons/io5";
+import { IoGrid, IoList } from 'react-icons/io5';
 
 const AvailableCar = () => {
   const [carsData, setCarsData] = useState([]);
   const [filter, setFilter] = useState('');
   const [search, setSearch] = useState('');
-  const [toggle, setToggle] = useState(false);
+  const [toggle, setToggle] = useState(true);
 
   useEffect(() => {
     const fetchAllCarsData = async () => {
@@ -44,8 +44,12 @@ const AvailableCar = () => {
         <div className='flex flex-col gap-4 md:flex-row'>
           {/* Sort By Category */}
           <div>
-            <select name='category' id='category' className='select' value={filter}
-              onChange={(event)=>setFilter(event.target.value)}
+            <select
+              name='category'
+              id='category'
+              className='select'
+              value={filter}
+              onChange={(event) => setFilter(event.target.value)}
             >
               <option value=''>Filter By Category</option>
               <option value='ascending'>Lowest First</option>
@@ -64,26 +68,39 @@ const AvailableCar = () => {
               placeholder='Enter car model'
               aria-label='Enter car model'
               value={search}
-              onChange={(event)=>setSearch(event.target.value)}
+              onChange={(event) => setSearch(event.target.value)}
             />
             <button className='btn join-item rounded-r-full'>Search</button>
           </div>
 
-          <div>a</div>
+          <div className='flex gap-4'>
+            <div>
+              <button className='btn' onClick={() => setToggle(!toggle)}>
+                {toggle === true ? <IoGrid size={20} /> : <IoList size={20} />}
+              </button>
+              {/* {toggle === true ? (<button></button>) : (<button></button>)} */}
+            </div>
 
-          {/* Reset */}
-          <div>
-            <button onClick={handleResetButton} className='btn'>
-              Reset
-            </button>
+            {/* Reset */}
+            <div>
+              <button onClick={handleResetButton} className='btn'>
+                Reset
+              </button>
+            </div>
           </div>
         </div>
 
-        {/* Card container */}
+        {/* Card container (grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4) */}
         <div>
-          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+          <div
+            className={`${
+              toggle === true
+                ? 'grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'
+                : 'lg:grid lg:grid-cols-1'
+            }`}
+          >
             {carsData?.map((car) => (
-              <AvailableCarsCard key={car?._id} car={car} />
+              <AvailableCarsCard key={car?._id} car={car} toggle={toggle} />
             ))}
           </div>
         </div>
